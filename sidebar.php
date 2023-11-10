bd1c5397c9bed7c3535142d6c4f281b1b2c46738
BiljanaDa <bibidakovic@gmail.com>
2023-11-10 16:17:14 +0100

    zadatak7

--- sidebar.php
+++ sidebar.php
@@ -1,31 +1,19 @@
+<?php include_once('db.php'); ?>
+
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
-            <div class="sidebar-module sidebar-module-inset">
-                <h4>About</h4>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            </div>
-            <div class="sidebar-module">
-                <h4>Archives</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">March 2014</a></li>
-                    <li><a href="#">February 2014</a></li>
-                    <li><a href="#">January 2014</a></li>
-                    <li><a href="#">December 2013</a></li>
-                    <li><a href="#">November 2013</a></li>
-                    <li><a href="#">October 2013</a></li>
-                    <li><a href="#">September 2013</a></li>
-                    <li><a href="#">August 2013</a></li>
-                    <li><a href="#">July 2013</a></li>
-                    <li><a href="#">June 2013</a></li>
-                    <li><a href="#">May 2013</a></li>
-                    <li><a href="#">April 2013</a></li>
-                </ol>
-            </div>
-            <div class="sidebar-module">
-                <h4>Elsewhere</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Facebook</a></li>
-                </ol>
-            </div>
+    <div class="sidebar-module sidebar-module-inset">
+        <h4>Latest posts</h4>
+        <ol class="list">
+            <?php
+
+            $latestPosts = "SELECT * FROM posts ORDER BY created_at DESC LIMIT 5";
+            $latest5 = getData($latestPosts, $connection,true);
+
+            foreach ($latest5 as $post) {
+                echo '<p><a href="single-post.php?post_id=' . $post['id'] . '">' . $post['title'] . '</a></p>';
+            }
+          ?>
+
+        </ol>
+    </div>
 </aside>
