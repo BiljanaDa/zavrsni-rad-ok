7f23229d0bdfe60bce5668901a795215b35cb692
BiljanaDa <bibidakovic@gmail.com>
2023-11-10 16:21:51 +0100

    zadatak8

--- comments.php
+++ comments.php
@@ -1,7 +1,22 @@
+<?php $postId = $_GET['post_id']; ?>
 <div class="comments">
+    <h3>Add your comment</h3>
+    <form action="create-comment.php" method="post" onsubmit="return validateForm();">
+        <div class="form-group">
+            <label for="author">Author:</label>
+            <input type="text" name="author" id="author" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="text">Comment:</label>
+            <textarea name="text" id="text" class="form-control" rows="4"></textarea>
+        </div>
+        <input type="hidden" name="post_id" value="<?= $postId ?>">
+        <button type="submit" class="btn btn-primary">Submit comment</button>
+    </form>
     <hr>
     <button class="btn btn-default" id="comments-btn">Hide comments</button>
     <h3>Comments</h3>
+
     <ul id="comments-list">
         <?php foreach ($comments as $comment) { ?>
             <li class="">
@@ -9,6 +24,8 @@
                     <?= $comment['author'] ?>
                 </h4>
                 <?= $comment['text'] ?>
+                <button class="btn btn-default"><a
+                        href="delete-comment.php?post_id=<?= $postId ?>&id=<?= $comment['id'] ?>">Delete</a></button>
             </li>
         <?php } ?>
     </ul>
--- script.js
+++ script.js
@@ -10,3 +10,13 @@ commentsBtn.addEventListener("click", () => {
     commentsBtn.innerHTML = "Hide comments";
   }
 });
+
+function validateForm() {
+  var author = document.getElementById("author").value;
+  var text = document.getElementById("text").value;
+
+  if (!author || !text) {
+    alert("You must fill in all of the fields!");
+    return false;
+  }
+}
